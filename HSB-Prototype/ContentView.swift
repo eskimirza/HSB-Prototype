045b35fd16454272033ce4f31fbfa9efa1be7e57
Eski Mirza <eski@jobadder.com>
2020-09-07 21:53:35 +1000

    Initial Commit

---
+++ HSB-Prototype/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HSB-Prototype
+//
+//  Created by Eski Mirza on 7/9/20.
+//  Copyright © 2020 Eski Mirza. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
